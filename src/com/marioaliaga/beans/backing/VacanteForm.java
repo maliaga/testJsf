f914807a30f55f6efa07789f137ca1fdf6e88fc3
Mario Aliaga Guzmán <maliaga@MacBookPro.home>
2016-08-27 07:52:16 -0300

    Add value change listaner on index

--- src/com/marioaliaga/beans/backing/VacanteForm.java
+++ src/com/marioaliaga/beans/backing/VacanteForm.java
@@ -6,7 +6,10 @@ import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ManagedProperty;
 import javax.faces.bean.RequestScoped;
+import javax.faces.component.UIInput;
+import javax.faces.component.UIViewRoot;
 import javax.faces.context.FacesContext;
+import javax.faces.event.ValueChangeEvent;
 
 /**
  * Created by maliaga on 8/19/16.
@@ -42,5 +45,26 @@ public class VacanteForm {
         } else {
             return "error";
         }
+    }
+
+    public void codigoPostalListener(ValueChangeEvent valueChangeEvent){
+        FacesContext facesContext = FacesContext.getCurrentInstance();
+        UIViewRoot uiViewRoot = facesContext.getViewRoot();
+        String newCodigoPostal = (String) valueChangeEvent.getNewValue();
+
+        if("123456".equals(newCodigoPostal)){
+            UIInput ciudadUiInput = (UIInput) uiViewRoot.findComponent("vacanteForm:ciudad");
+            String ciudad = "Santiago";
+            ciudadUiInput.setValue(ciudad);
+            ciudadUiInput.setSubmittedValue(ciudad);
+
+            UIInput coloniaUiInput = (UIInput) uiViewRoot.findComponent("vacanteForm:colonia");
+            String colonia = "Metropolitana";
+            coloniaUiInput.setValue(colonia);
+            coloniaUiInput.setSubmittedValue(colonia);
+
+            facesContext.renderResponse();
+        }
+
     }
 }
